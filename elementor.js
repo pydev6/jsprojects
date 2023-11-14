16e72ea71e2f3b1c89608c0cb75b4d60f8ed05ae
pydev6 <devpy6@gmail.com>
2023-11-14 16:43:01 -0600

    Added netflex logo animation

---
+++ elementor.js
@@ -0,0 +1,33 @@
+// <style>.swiper-slide-image{cursor: pointer;}
+
+// .swiper-slide .swiper-slide-duplicate{
+//     width:100%;
+//     height:350px;
+
+// }
+//  </style>
+
+// /* Edit the links HERE */
+// let imgs= document.querySelectorAll(".swiper-slide-image");
+// let links = [
+//     'http://locahost/cie-dfw/all-evetnts/',
+//     'https://www.google.com',
+//     'https://www.twitter.com',
+//     'https://www.facebook.com',
+
+// ];
+// for(let i;i<links.length;i++){
+//     imgs[i].addEventListener("click",()=>{
+//     window.location.href=links[i]})
+// }
+
+// // document.addEventListener('DOMContentLoaded', function () {
+// // document.body.addEventListener('click', function (e) {
+// //     if (e.target.closest('.carousel1 .swiper-slide')){
+// //     let i = e.target.closest('.swiper-slide').getAttribute("data-swiper-slide-index");
+// //     if (links[i] && links[i].length > 2){
+// //     window.open(links[i]); /* TO OPEN LINKS IN SAME WINDOW USE: location = links[i]; */
+// //     }
+// //     }
+// // });
+// // });
--- js_dom.js
+++ js_dom.js
@@ -4,24 +4,38 @@
 
 // element.addEventListner("click", function)
 
-const buttonTwo = document.querySelector(".btn-2");
-const cardEl = document.querySelector(".card-container");
+const buttonTwo = document.querySelector('.btn-2');
+const cardEl = document.querySelector('.card-container');
 console.log(cardEl);
 
 function changeBg() {
-  cardEl.style.backgroundColor = "red";
+  cardEl.style.backgroundColor = 'red';
 }
 
-cardEl.addEventListener("mouseover", changeBg);
+cardEl.addEventListener('mouseover', changeBg);
 
-const hiddenContent = document.querySelector(".hidden-content");
-const revealBtn = document.querySelector(".reveal-btn");
+const hiddenContent = document.querySelector('.hidden-content');
+const revealBtn = document.querySelector('.reveal-btn');
 function revealContent() {
-  if (hiddenContent.classList.contains("boxes")) {
-    hiddenContent.classList.remove("boxes");
+  if (hiddenContent.classList.contains('boxes')) {
+    hiddenContent.classList.remove('boxes');
   } else {
-    hiddenContent.classList.add("boxes");
+    hiddenContent.classList.add('boxes');
   }
 }
 
-revealBtn.addEventListener("click", revealContent);
+revealBtn.addEventListener('click', revealContent);
+
+var moresponsorBtn = document.getElementById('sponsorBtn');
+moresponsorBtn.addEventListener('click', showMore);
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById('hide-header').style.top = '0';
+  } else {
+    document.getElementById('hide-header').style.top = '-80 px'; /* adjust this value to the height of your header */
+  }
+  prevScrollpos = currentScrollPos;
+};
